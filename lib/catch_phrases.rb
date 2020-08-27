2936af01348cf77f3bb314d3201c670f24116a54
Elias Delgado <evdelgado99@gmail.com>
2020-08-27 07:09:21 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,22 +4,16 @@ def all_phrases
     phrase = "It's-a me, Mario!"
     puts phrase
   end
-  
-  puts mario
 
   def toadstool
     status = "Thank You Mario! But Our Princess Is In Another Castle!"
     puts status
   end
-  
-  puts toadstool
 
   def link
     hero = "It's Dangerous To Go Alone! Take This."
     puts hero
   end
-  
-  puts link
 
 end
 
