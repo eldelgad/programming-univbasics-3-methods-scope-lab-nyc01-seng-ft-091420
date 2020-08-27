0dc500bbac58dbc63498b0d1583fa25252eda051
Elias Delgado <evdelgado99@gmail.com>
2020-08-27 07:07:24 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,14 +1,18 @@
-def mario
-  phrase = "It's-a me, Mario!"
-  puts phrase
-end
+def all_phrases
 
-def toadstool
-  status = "Thank You Mario! But Our Princess Is In Another Castle!"
-  puts status
-end
+  def mario
+    phrase = "It's-a me, Mario!"
+    puts phrase
+  end
 
-def link
-  hero = "It's Dangerous To Go Alone! Take This."
-  puts hero
+  def toadstool
+    status = "Thank You Mario! But Our Princess Is In Another Castle!"
+    puts status
+  end
+
+  def link
+    hero = "It's Dangerous To Go Alone! Take This."
+    puts hero
+  end
+
 end
